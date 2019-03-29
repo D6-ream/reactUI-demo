53c50c845a8a9df180df0af1a3ecfc733a90a2b8
sam.mj <sam.mj@alibaba-inc.com>
2019-03-29 10:53:01 +0800

    feat: demo init

---
+++ src/Test/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import debug from 'debug';
+
+const log = debug('reactUI:Test');
+
+export interface TestInfo {
+  infoString: string,
+  infoNumber: number,
+}
+
+export interface TestProps {
+  propsString: string,
+  propsObject: TestInfo,
+  propsNumber?: number,
+}
+
+export interface TestState {
+  stateString: string,
+}
+
+class Test extends React.Component<TestProps, TestState> {
+  static displayName = 'Test';
+
+  constructor(props: TestProps) {
+    super(props);
+    this.state = {
+      stateString: '',
+    };
+  }
+
+  render() {
+    const { stateString } = this.state;
+    const { propsString, propsObject, propsNumber } = this.props;
+    return (
+      <div>
+        <div>{stateString}</div>
+        <div>{propsString}</div>
+        <div>{propsObject}</div>
+        <div>{propsNumber}</div>
+      </div>
+    )
+  }
+}
